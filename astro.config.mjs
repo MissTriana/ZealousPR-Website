304edc3d28791d592e8ae8e2a78bb1147209c005
MissTriana <yasmine.triana@gmail.com>
2026-08-15 12:53:40 +0100

    Delete astro.config.mjs

--- astro.config.mjs
+++
@@ -1,8 +0,0 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
-
-export default defineConfig({
-  site: 'https://www.zealouspr.com',
-  integrations: [sitemap()],
-});
